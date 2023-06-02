f72ce9ae7335b691352e1fa1c0e977cd468cd70b
Ansh <anshmishra703@gmail.com>
2023-06-03 01:08:11 +0530

    Created HTML

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $currentDateTime = date('Y-m-d H:i:s');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            LAMP Stack Project
+        </title>
+    </head>
+    <body>
+        <h1>Current Date and time : <?php echo $currentDateTime; ?></h1>
+    </body>
+</html>
