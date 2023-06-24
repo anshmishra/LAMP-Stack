1965aec6b0fe1cd7a57f867945d6ef3631e1b952
Ansh <anshmishra703@gmail.com>
2023-06-24 13:56:46 +0530

    update to index.php

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ $connect = mysqli_connect(
     'lamp_stack'
 );
 
-$query = 'SELECT * FROM blog';
+$query = 'SELECT * FROM table';
 $result - mysqli_query($connect, $query);
 
 echo '<h1>MySQL Content,</h1>';
 
 while($record = mysqli_fetch_assoc($result))
 {
-    echo'<h2>'.$record['title'].'</h2>';
-    echo'<p>'.$record['content'].'</p>';
-    echo 'Posted: '.$record['date'];
+    echo'<h2>'.$record['Title'].'</h2>';
+    echo'<p>'.$record['Body'].'</p>';
+    echo 'Posted: '.$record['Date'];
     echo'<hr>';
 }
